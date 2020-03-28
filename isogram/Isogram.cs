92d594377bb098b5f84a4b7874868b908adc29d0
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-03-28 15:05:01 -0300

    Add Isogram

---
+++ isogram/Isogram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        if (string.IsNullOrEmpty(word)) return true;
+
+        var chars = word.Replace(" ", "")
+            .Replace("-", "")
+            .ToLower()
+            .ToCharArray();
+
+        foreach (var c in chars)
+        {
+            if (chars.Count(x => x == c) > 1)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
