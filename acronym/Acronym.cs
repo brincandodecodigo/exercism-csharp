488785c2687a7c08219d0dfd263b7ebb18272980
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-03-28 16:22:55 -0300

    Add acronym

---
+++ acronym/Acronym.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var chars = phrase
+            .Replace("-", " ")
+            .Replace("_", " ")
+            .Trim()
+            .ToCharArray();
+
+        var acronym = new StringBuilder(chars[0].ToString().ToUpper());
+        for (int i = 0; i < chars.Count(); i++)
+        {
+            if (char.IsWhiteSpace(chars[i]))
+            {
+                var abbreviateChar = chars[i + 1];
+                if (char.IsLetter(abbreviateChar))
+                {
+                    acronym.Append(abbreviateChar.ToString().ToUpper());
+                }
+            }
+        }
+
+        return acronym.ToString();
+    }
+}
