216d66f35f20e929658772c45e9a52dcb83fd2ff
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-03-28 14:15:19 -0300

    Add pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    static char[] alphabet = new[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    public static bool IsPangram(string input)
+    {
+        if (string.IsNullOrEmpty(input)) return false;
+
+        var loweredInput = input.ToLowerInvariant();
+        return alphabet.All(c => loweredInput.Contains(c));
+    }
+}
